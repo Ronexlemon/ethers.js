d72cd4b663f902edee22095a70e44ba626a390bd
RonexLemon <ronexondimu@gmail.com>
2024-03-12 16:08:04 +0300

    get block

---
+++ index.js
@@ -0,0 +1,7 @@
+const {ethers} = require("ethers")
+
+const provider = new ethers.JsonRpcProvider(); // connect to port 8545 localhoast 
+
+(async function  connect(){
+    console.log(await provider.getBlock())// get blocknumber
+})()
