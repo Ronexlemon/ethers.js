7109610c099f8acd517fed628b4650991fe61b75
Ronex Ondimu <68167040+Ronexlemon@users.noreply.github.com>
2024-03-29 12:26:43 +0300

    Update wallet.js

--- wallet/wallet.js
+++ wallet/wallet.js
@@ -30,6 +30,27 @@ const json = wallet.encrypt(password, {
    return json;
 }
 
+const encrypt2 = async()=>{
+    // Our wallet object
+const wallet = Wallet.createRandom();
+
+// The password to encrypt with
+const password = "password123";
+
+// WARNING: Doing this substantially reduces the security
+//          of the wallet. This is highly NOT recommended.
+
+// We override the default scrypt.N value, which is used
+// to indicate the difficulty to crack this wallet.
+const json = wallet.encrypt(password, {
+  scrypt: {
+    // The number must be a power of 2 (default: 131072)
+    N: 64
+  }
+});
+   return json;
+}
+
 //create wallet from baseWallet
 
 const  createFromMnemonic=async(entropy,password)=>{
